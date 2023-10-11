eac6d3b1f83fd8b4b3c6f1e62c3f6a770394dc66
henry9405 <144359211+henry9405@users.noreply.github.com>
2023-10-11 18:34:28 +0300

    event listener

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+//function addingEventListener() {
+//}
+
+
 function addingEventListener() {
-}
+    // Get a reference to the HTML element with the id "myButton"
+    const input = document.getElementById('input');
+  
+    // Define a function to be called when the event is triggered
+    function myEventListener () {
+      alert('I was clicked');
+    };
+    input.addEventListener('click', myEventListener);
+    }
+  
+    // Use addEventListener to bind the event listener to the element
+  
+  
+  // Call the addingEventListener function to set up the event listener
+
+  
